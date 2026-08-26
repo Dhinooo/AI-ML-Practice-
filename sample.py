31e217b874c5b8f6d90636ed2d7a1c2ea4d00df5
Dhinooo <karandhina1910@gmail.com>
2026-08-26 08:45:15 +0530

    python variable

---
+++ sample.py
@@ -0,0 +1,12 @@
+name = "dhina" 
+
+age = 25  #integer 
+
+persentage = 9.07   #float 
+
+isdeveloper = True  #bool
+
+print("User Name Is" ,name )
+print("Your Age Is :", age)
+print("Persentage Is: ", persentage)
+print("Are You Developer ", isdeveloper)
---
+++ variable.py
@@ -0,0 +1,10 @@
+name="dhina"
+age=25
+salary="one Hundred lachk rupees"
+bata = False
+
+print("User Name is :",name)
+print("Your Age is :",age)
+print("Your Salary is :",salary)
+print("Your bata is :",bata)
+
