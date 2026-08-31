4e6c315d32978b1297cc74f1e3f746bb2de75c85
Dhinooo <karandhina1910@gmail.com>
2026-08-31 07:56:43 +0530

    python type_casting

---
+++ type_casting.py
@@ -0,0 +1,57 @@
+# a = "123abc"
+
+
+# b=int(a)
+
+# b=str(a)
+
+# print(b)
+
+# b = 123
+
+# c=int(a)
+
+# d=str(b)
+# print(f"the data type is {type(d)}",d)
+
+# print(f"the data type is {type(c)}",c)
+
+# g=float(d)
+# print(f"the data type is {type(g)}",g)
+
+# print(f"the data type is {type(d)}",d)
+# print(type(d))
+
+
+#String
+
+a=True
+b=str(a)
+print(b)
+
+# o/p True
+
+
+#int
+
+a=True
+b=int(a)
+print(b)
+
+# o/p 1
+
+#float
+
+a=True
+b=float(a)
+print(b)
+
+# o/p 1.0
+
+#bool
+
+a=True
+b=bool(a)
+print(b)
+
+# o/p True
--- variable.py
+++ variable.py
@@ -1,10 +1,14 @@
-name="dhina"
-age=25
-salary="one Hundred lachk rupees"
-bata = False
+name="dhina"   #string 
+age=25         #int 
+salary= 13000.00 # float 
+bata = False  #bool
 
 print("User Name is :",name)
 print("Your Age is :",age)
 print("Your Salary is :",salary)
 print("Your bata is :",bata)
 
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(bata))
