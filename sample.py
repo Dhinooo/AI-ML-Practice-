c016f088a1b9f2c8ee6bb245805c1067effa4a92
Dhinooo <karandhina1910@gmail.com>
2026-09-06 19:35:16 +0530

    python variables

--- sample.py
+++ sample.py
@@ -6,7 +6,14 @@ persentage = 9.07   #float
 
 isdeveloper = True  #bool
 
-print("User Name Is" ,name )
-print("Your Age Is :", age)
-print("Persentage Is: ", persentage)
-print("Are You Developer ", isdeveloper)
+# print("User Name Is" ,name )
+# print("Your Age Is :", age)
+# print("Persentage Is: ", persentage)
+# print("Are You Developer ", isdeveloper)
+
+
+user_name = str(input("Enter User Name : "))
+
+user_age = int(input("Enter Your Age : "))
+print(f"Welcome {user_name} here is Python Programming")
+print(f"Your age is {user_age} once again {user_name}welcome ")
