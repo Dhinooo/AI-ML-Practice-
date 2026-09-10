f2624ee615979b21950e4b2df4862e4598df9d47
Dhinooo <karandhina1910@gmail.com>
2026-09-10 21:11:06 +0530

    python Exercise-if-else

--- exercise.py
+++ exercise.py
@@ -10,3 +10,11 @@ elif number < 0:
     print("The number is negative", number)
 else:
     print("The number is Zero")
+number = int(input("Enter a Number : "))
+
+if number > 0:
+    print("The number is positive ", number)
+elif number < 0:
+    print("The number is negative", number)
+else:
+    print("The number is Zero")
