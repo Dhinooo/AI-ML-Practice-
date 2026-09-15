e21f3af5c74da9f083f97d350c3e49777e53a92e
Dhinooo <karandhina1910@gmail.com>
2026-09-15 08:07:19 +0530

    python dictonary_example

---
+++ dictionary_example.py
@@ -0,0 +1,12 @@
+students = {
+    "name": "dhina",
+    "age": 25,
+    "course": "python",
+    "Marks": [20, 40, 20, 40, 20],
+}
+
+# print(students["name"])
+# print(students["age"])
+# print(students.keys())
+print(students.get("age"))
+# print(students.values())
