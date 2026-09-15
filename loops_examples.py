23f268de3f42667d0ff2d8fd0c66e3b1c8fcb610
Dhinooo <karandhina1910@gmail.com>
2026-09-15 08:38:56 +0530

    python for_loops

---
+++ loops_examples.py
@@ -0,0 +1,40 @@
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+# print("--------------------------")
+
+# for i in range(30):
+#     print("Hello World :",i)
+
+# fruits = ['apple','Orange','mango','papaya']
+
+# for i in fruits:
+#     print('The Fruit Name is : ',i)
+
+
+# stu_name ="I am the student, my name is dhina "
+
+# for i in stu_name:
+#     print("Student Name is : ",i)
+
+# for i in range(1, 30, 2):
+#     print("HELLO", i)
+
+
+students = {
+    "name": "dhina",
+    "age": 25,
+    "course": "python",
+    "Marks": [20, 40, 20, 40, 20],
+}
+
+# for key,value in students.items():
+#     print("The key is :",key, "....","The value is :",value)
+
+
+for key in students.keys():
+    print("The key is :",key)
+
+# for value in students.values():
+#     print("The value is :",value)
