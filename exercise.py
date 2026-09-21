480752696e4f5ad59dc98baf91db92ee6c5ca014
Dhinooo <karandhina1910@gmail.com>
2026-09-21 22:01:17 +0530

    exercise

--- exercise.py
+++ exercise.py
@@ -46,3 +46,147 @@
 # plt.title("Student Data")
 # plt.legend()
 # plt.show()
+
+# a = 10
+# b = 20
+
+# a, b = b, a  # 10, 20 = 20, 10
+
+# print(a) # 10
+# print(b) # 20
+
+# name = "Dhinakaran"
+# age = 25
+
+# print("My name is", name)
+# print("I am", age, "years old")
+
+
+# name = "Dhinakaran"
+# age = 25
+# age = age + 1
+
+# print(name)
+# print(age)
+
+
+# a = 10
+# b = 10.5
+# c = "10"
+# d = True
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+x = 10
+y = 5.5
+
+print(x + y)
+print(type(x + y))
+a = "10"
+b = "20"
+
+print(a + b)
+
+
+a = 10
+b = "20"
+
+print(str(a) + b)
+
+
+a = "10"
+b = "20"
+
+print(int(a) + int(b))
+
+
+is_student = True
+
+print(is_student)
+print(type(is_student))
+
+
+x = True
+y = 10
+
+print(x + y)
+
+
+x = False
+y = 10
+
+print(x + y)
+
+
+x = 10
+y = float(x)
+
+print(y)
+print(type(y))
+
+x = 10.5
+y = int(x)
+
+print(y)
+print(type(y))
+
+x = "10.5"
+y = float(x)
+
+print(y)
+print(type(y))
+
+
+x = "10"
+y = 5
+
+print(x + str(y))
+
+a = 10
+b = 2.5
+c = "5"
+
+result = a + b + int(c)
+
+print(result)
+print(type(result))
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+
+a = 10
+b = 3
+
+print(a / b)
+
+a = 10
+b = 3
+
+print(a % b)
+
+a = 10
+b = 3
+
+print(a // b)
+
+a = 10
+b = 3
+
+result = a + b * 2
+
+print(result)
+
+correct = 87
+total = 100
+
+accuracy = (correct / total) * 100
+
+print(accuracy)
